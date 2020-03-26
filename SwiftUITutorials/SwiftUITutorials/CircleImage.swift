35559f277ca78bebeb58e6d9ea1062c2d2815a83
Amaury A V A Souza <amauryavasouza@gmail.com>
2020-03-26 17:41:18 -0300

    tutorial completo :)

---
+++ SwiftUITutorials/SwiftUITutorials/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  SwiftUITutorials
+//
+//  Created by Amaury A V A Souza on 26/03/20.
+//  Copyright © 2020 Amaury A V A Souza. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- SwiftUITutorials/SwiftUITutorials/ContentView.swift
+++ SwiftUITutorials/SwiftUITutorials/ContentView.swift
@@ -4,13 +4,34 @@
 //
 //  Created by Amaury A V A Souza on 26/03/20.
 //  Copyright © 2020 Amaury A V A Souza. All rights reserved.
-//
+// tutorial https://developer.apple.com/tutorials/swiftui/creating-and-combining-views
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            MapView()
+                .frame(height:300)
+                .edgesIgnoringSafeArea(.top)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading){
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }.padding()
+            Spacer()
+        }
+        
+            
     }
 }
 
