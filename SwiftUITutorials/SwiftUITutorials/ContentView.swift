60efd3ec0f7227b4448e62bac50a7dcf64e5f271
Amaury A V A Souza <amauryavasouza@gmail.com>
2020-03-26 14:45:27 -0300

    criação do projeto

---
+++ SwiftUITutorials/SwiftUITutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorials
+//
+//  Created by Amaury A V A Souza on 26/03/20.
+//  Copyright © 2020 Amaury A V A Souza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
